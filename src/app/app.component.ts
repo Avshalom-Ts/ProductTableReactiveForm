420df1c7f8d02535194e85c18a36dfe1b03a7a65
Azdevpp <Azdevpp@gmail.com>
2023-01-14 19:41:29 +0200

    getAllProducts Methode is workind in console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddProductDialogComponent } from './add-product-dialog/add-product-dialog.component';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(public dialog: MatDialog) {}
+export class AppComponent implements OnInit {
+  constructor(public dialog: MatDialog, private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
 
   openDialog() {
     const dialogRef = this.dialog.open(AddProductDialogComponent, {
@@ -17,6 +22,17 @@ export class AppComponent {
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  getAllProducts() {
+    this.api.getProduct().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: () => {
+        alert('Error while fetching the Records');
+      },
     });
   }
 }
